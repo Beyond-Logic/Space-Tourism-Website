4162c3919be3ee48ed4c93e72855be4b3e713d13
Beyond-Logic <edemaero@gmail.com>
2023-03-06 00:33:33 +0100

    Styling, tailwindCSS config, folder structure, layout updates

---
+++ src/Layouts/Container.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Container = ({ children }) => {
+  return <div className="max-w-screen-2xl m-auto bg-primary">{children}</div>;
+};
+
+export default Container;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "background-home-desktop":
+          "url('/src/assets/home/background-home-desktop.jpg')",
+      },
+    },
+    colors: {
+      primary: "#0B0D17",
+      secondary: "#D0D6F9",
+      white: "#FFFFFF",
+    },
   },
   plugins: [],
 };
