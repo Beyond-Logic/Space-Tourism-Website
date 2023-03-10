58fa1544904bed52b4eb4d2c44aa97869f4656b6
Beyond-Logic <edemaero@gmail.com>
2023-03-10 15:16:30 +0100

    feat:style updates

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,17 +2,17 @@ import React, { useState } from "react";
 import Logo from "../assets/shared/logo.svg";
 import IconHamburger from "../assets/shared/icon-hamburger.svg";
 import IconClose from "../assets/shared/icon-close.svg";
-import { NavLink } from "react-router-dom";
-import { useLocation } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 
 const NavBar = () => {
   const [toggleMobileMenu, settoggleMobileMenu] = useState(false);
-  const { pathname } = useLocation();
 
   return (
     <div className="flex justify-between items-center relative xl:pt-10 pt-0 px-10 md:px-0 ">
       <div className="flex md:pl-[55px] justify-between items-center gap-16 ">
-        <img src={Logo} alt="Logo" className="w-full" />
+        <Link to="/">
+          <img src={Logo} alt="Logo" className="w-full" />
+        </Link>
         <div className="navbar_divider absolute xl:max-w-[473px] left-[170px] xl:block hidden"></div>
       </div>
       <div className="navbar flex  justify-center items-center lg:max-w-[830px] lg:w-full px-0 md:px-10 ">
